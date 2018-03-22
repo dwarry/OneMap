22d09cec4a8ffceaedeb6e245140bb93a740ea6a
Dave Warry <dave@warry.org.uk>
2018-03-22 17:49:50 +0000

    Add Persistence class for loading and saving changes

---
+++ OneMap/OneNote/Persistence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMap.OneNote
+{
+    public interface IPersistence
+    {
+        /// <summary>
+        /// Get the currently available notebooks
+        /// </summary>
+        /// <returns></returns>
+        Notebooks LoadNotebooks();
+
+        Page GetPage(string pageId);
+
+
+        Page GetCurrentPage();
+
+
+        void UpdatePage(Page page);
+    }
+
+    public class OneNotePersistence : IPersistence
+    {
+
+    }
+}
